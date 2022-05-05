c2e5edb089e834e61530d452a7efeb8b33188964
francis kamande <franciskamande2001@gmail.com>
2022-05-05 09:46:56 +0300

    A new SimUnit object and add it into the ArrayList in the constructor arguments

--- RetenetionBot.java
+++ RetenetionBot.java
@@ -4,6 +4,7 @@ import java.util.*;
 class RetenetionBot{
 	//Constructor taking in an ArrayList
 	RetenetionBot(ArrayList rlist){
-		
+		//Make a new SimUnit object and add it into the ArrayList in the arguments
+		rlist.add(new SimUnit("Retention"));
 	}
 }
