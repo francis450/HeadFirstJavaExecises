70c1bb5e37fa7497728602552062dac42061154c
francis kamande <franciskamande2001@gmail.com>
2022-05-06 10:09:38 +0300

    Return the new GC object

--- GC.java
+++ GC.java
@@ -3,6 +3,10 @@ public class GC{
 	//First doStuff() method 
 	public static GC doStuff(){
 		//Make an instance of the GC class
-		GC newGC = new GC();
+		GC newGC = new GC();	
+		//Call the doStuff2() method on the new object
+		doStuff2(newGC);
+		//Return the new GC object
+		return newGC;
 	}
 }
