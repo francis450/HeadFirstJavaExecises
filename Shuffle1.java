87f1ed26aabf435fa154e30fd47dc5ba230467a8
francis kamande <franciskamande2001@gmail.com>
2022-05-08 10:36:22 +0300

    execute the curly braces if x greater than two

--- Shuffle1.java
+++ Shuffle1.java
@@ -4,6 +4,11 @@ class Shuffle1{
 	public static void main(String [] args){
 		//make an int and initialise it to 3
 		int x = 3;
+
+		//execute the curly braces if x greater than two
+		if( x > 2){
+
+		}
 	}
 
 }
