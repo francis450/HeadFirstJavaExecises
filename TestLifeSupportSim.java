273150e8e9a8fe9591c1a2cd73a41d4d3e550449
francis kamande <franciskamande2001@gmail.com>
2022-05-12 11:24:53 +0300

    a SimUnit type ArrayList reference and assign it an actual Arraylist

--- TestLifeSupportSim.java
+++ TestLifeSupportSim.java
@@ -1,2 +1,9 @@
 //import java utilities package
 import java.util.*;
+
+class TestLifeSupportSim{
+	public static void main(String [] args){
+		//Make a SimUnit type ArrayList reference and assign it an actual Arraylist
+		ArrayList<SimUnit> aList = new ArrayList<SimUnit>();
+	}
+}
