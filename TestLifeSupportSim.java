b6732691a22bad563cc160e6b796ec7a7ece6ca9
francis kamande <franciskamande2001@gmail.com>
2022-05-13 11:26:15 +0300

    a new V2radiator and assign it a reference v2

--- TestLifeSupportSim.java
+++ TestLifeSupportSim.java
@@ -5,5 +5,7 @@ class TestLifeSupportSim{
 	public static void main(String [] args){
 		//Make a SimUnit type ArrayList reference and assign it an actual Arraylist
 		ArrayList<SimUnit> aList = new ArrayList<SimUnit>();
+		//Make a new V2radiator and assign it a reference v2
+		V2Radiator v2 = new V2Radiator(aList);	
 	}
 }
