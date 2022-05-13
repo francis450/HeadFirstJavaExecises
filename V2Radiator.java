8b69b26739bf0d59b8b35fcb732a175c5388de4d
francis kamande <franciskamande2001@gmail.com>
2022-05-13 11:32:57 +0300

    a V2Radiator constructor to take in an ArrayList in as parameters

--- V2Radiator.java
+++ V2Radiator.java
@@ -1,2 +1,8 @@
 //import java utities package
 import java.util.*;
+public class V2Radiator{
+	//Make a V2Radiator constructor to take in an ArrayList in as parameters
+	V2Radiator(ArrayList list){
+
+	}
+}
