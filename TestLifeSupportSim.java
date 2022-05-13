64e482ff7f126a53981206f1b86500ccfff4e853
francis kamande <franciskamande2001@gmail.com>
2022-05-13 11:27:36 +0300

    20 RetenetionBots' all with the same reference and pass to their constructor an ArrayList

--- TestLifeSupportSim.java
+++ TestLifeSupportSim.java
@@ -9,5 +9,10 @@ class TestLifeSupportSim{
 		V2Radiator v2 = new V2Radiator(aList);	
 		//MAke a new V#Radiator and assign it a reference v3
 		V3Radiator v3 = new V3Radiator(aList);
+
+		for(int z = 0; z < 20; z++){
+			//Make 20 RetenetionBots' all with the same reference and pass to their constructor an ArrayList
+			RetenetionBot ret = new RetenetionBot(aList); 
+		}
 	}
 }
