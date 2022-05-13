ba765b2a0a5547d41909569dc1859c12a65225d0
francis kamande <franciskamande2001@gmail.com>
2022-05-13 11:33:48 +0300

    a new SimUnit object and add it to the construcot in the arguments

--- V2Radiator.java
+++ V2Radiator.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class V2Radiator{
 	//Make a V2Radiator constructor to take in an ArrayList in as parameters
 	V2Radiator(ArrayList list){
-
+		for(int x=0;x<5;x++){
+			//Make a new SimUnit object and add it to the construcot in the arguments
+			list.add(new SimUnit("V2Radiator"));
+		}
 	}
 }
