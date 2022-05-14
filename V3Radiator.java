10993f76f84eb3cd8a22af075484fe23112367ff
francis kamande <franciskamande2001@gmail.com>
2022-05-14 11:41:01 +0300

    Pass the ArrayList in the constructor arguments to the superclass constructor(V2Radiator)

--- V3Radiator.java
+++ V3Radiator.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class V3Radiator extends V2Radiator{
 	//Make a V3Radiator constructor that takes in an ArrayList in the arguments
 	V3Radiator(ArrayList lglist){
-
+		//Pass the ArrayList in the constructor arguments to the superclass constructor(V2Radiator)
+		super(lglist);
 	}
 }
