e4dc659dae54196a3fdf24da79a68343f7624aa1
francis kamande <franciskamande2001@gmail.com>
2022-05-14 11:40:03 +0300

    a V3Radiator constructor that takes in an ArrayList in the arguments

--- V3Radiator.java
+++ V3Radiator.java
@@ -1,2 +1,9 @@
 //Import java utilities package
 import java.util.*;
+
+public class V3Radiator extends V2Radiator{
+	//Make a V3Radiator constructor that takes in an ArrayList in the arguments
+	V3Radiator(ArrayList lglist){
+
+	}
+}
