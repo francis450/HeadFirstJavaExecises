a126a57205df8ed2121a93271ae81a7df205dd3b
francis kamande <franciskamande2001@gmail.com>
2022-05-14 11:43:11 +0300

    a new SimUnit object and add  it into the ArrayList in the arguments

--- V3Radiator.java
+++ V3Radiator.java
@@ -6,5 +6,9 @@ public class V3Radiator extends V2Radiator{
 	V3Radiator(ArrayList lglist){
 		//Pass the ArrayList in the constructor arguments to the superclass constructor(V2Radiator)
 		super(lglist);
+		for(int i = 0; i < 10; i++){
+			//make a new SimUnit object and add  it into the ArrayList in the arguments
+			lglist.add(new SimUnit("V3Radiator"));
+		}
 	}
 }
